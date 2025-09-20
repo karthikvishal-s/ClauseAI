d7ab10c810cab8fca7ede16dae4598fd88ba63d0
Karthik Vishal <karthikvishal1506@gmail.com>
2025-09-20 20:54:53 +0530

    tscofig changed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ToastProvider, ToastViewport } from "@/components/toast"
+
 
 
 export const metadata: Metadata = {
